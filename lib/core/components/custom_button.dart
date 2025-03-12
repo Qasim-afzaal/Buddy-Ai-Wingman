e439e0f40496a39d81e240299a5aa105aceca67b
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-13 04:43:23 +0500

    add custom button

---
+++ lib/core/components/custom_button.dart
@@ -0,0 +1,30 @@
+
+import 'package:buddy_ai_wingman/core/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomButton extends StatelessWidget {
+  final void Function()? onTap;
+  final String buttonText;
+  final Color? backGroundColor;
+  final Color? textColor;
+  const CustomButton({super.key, this.onTap, required this.buttonText, this.backGroundColor, this.textColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+        child: Container(
+          alignment: Alignment.center,
+          height: 50,
+          width: MediaQuery.sizeOf(context).width,
+          decoration: BoxDecoration(
+            border: backGroundColor != null ? Border.all(color: AppColors.blackColor) : null,
+            color: backGroundColor ?? AppColors.blackColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Text(buttonText, style: GoogleFonts.interTight(textStyle: TextStyle(color: textColor ?? AppColors.whiteColor, fontWeight: FontWeight.w500, fontSize: 12, )),),
+        )
+    );
+  }
+}
