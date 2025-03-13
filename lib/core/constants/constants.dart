5154459536f74e9dfbee15a1ab8e5a0e86a27593
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-14 02:14:25 +0500

    add app constants

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,52 @@
+import 'package:buddy_ai_wingman/core/constants/get_storage.dart';
+import 'package:buddy_ai_wingman/core/constants/helper.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+
+GetStorageData getStorageData = GetStorageData();
+Utils utils = Utils();
+
+class Constants {
+  /// Socket
+  static IO.Socket? socket;
+  static const String socketBaseUrl = "";
+
+  /// Socket emit event
+  static const String sendMessage = 'sendMessage';
+  static const String getConversationDetail = 'getConversationDetail';
+  static const String getSuggestionAgain = 'getSuggestionAgain';
+  static const String manualChat = 'manualChat';
+
+  /// Socket Listening event
+  static const String receiveMessage = 'receiveMessage';
+  static const String receiveConversation = 'receiveConversation';
+  static const String suggestion = 'suggestion';
+  static const String message = 'message';
+  static const String error = 'Error';
+
+  /// API
+  static const String baseUrl = '';
+
+  /// Users API end point
+  static const String signUp = 'users/register';
+
+  /// Auth API end point
+  static const String login = 'auth/login';
+  static const String loginByPin = 'auth/login-by-pin';
+  static const String sendOTP = 'auth/send-otp';
+  static const String sendOTPforEmail = 'auth/send-email-verification';
+  static const String verifyOTP = 'auth/verify-otp';
+  static const String createPin = 'auth/create-pin';
+  static const String updatePin = 'auth/update-pin';
+  static const String forgetPassword = 'auth/forget-password';
+
+  /// Chat API end point
+  static const String withFile = 'chat/with-file';
+  static const String uploadFile = 'chat/upload-file';
+  static const String buddy_ai_wingmanLines = 'chat/buddy_ai_wingman-lines';
+  static const String archiveChat = 'chat/archive-chat';
+  static const String chatList = 'chat/chat-list/';
+  static const String deleteSingle = 'chat/single/';
+  static const String clearAll = 'chat/clear-all';
+    static const String deleteUser = 'users/';
+}
