83f157ceb1d8b56807f15a6bcdcf992d1a26aee4
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-14 02:15:13 +0500

    exports all import in one file

---
+++ lib/core/constants/imports.dart
@@ -0,0 +1,32 @@
+export 'package:flutter/material.dart';
+
+export 'package:buddy_ai_wingman/core/constants/app_strings.dart';
+export 'package:buddy_ai_wingman/core/components/sb.dart';
+export 'package:buddy_ai_wingman/gen/assets.gen.dart';
+export 'package:buddy_ai_wingman/core/constants/app_strings.dart';
+export 'package:buddy_ai_wingman/core/components/app_button.dart';
+export 'package:buddy_ai_wingman/core/components/app_bar.dart';
+export 'package:buddy_ai_wingman/core/components/app_text_field.dart';
+
+///Enums
+export 'package:buddy_ai_wingman/core/enums/genders.dart';
+export 'package:buddy_ai_wingman/core/enums/personality.dart';
+
+export 'package:buddy_ai_wingman/core/enums/chat_type.dart';
+export 'package:buddy_ai_wingman/models/message_model.dart';
+
+/// Pages=
+
+/// Widgets=
+
+/// Extensions
+export 'package:buddy_ai_wingman/core/extensions/build_context_extension.dart';
+
+/// PACKAGE
+export 'package:get/get.dart'
+    hide ContextExtensionss, FormData, MultipartFile, Response;
+export 'package:flutter_svg/flutter_svg.dart';
+
+export 'package:buddy_ai_wingman/core/constants/constants.dart';
+export 'package:buddy_ai_wingman/pages/dashboard/dashboard.dart';
+
