cadd3c1bd6e2b98f219d1dcf16cb026a0a195d4e
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-16 04:37:19 +0500

    add custom widget

---
+++ lib/core/Utils/custom_enums.dart
@@ -0,0 +1,17 @@
+enum UserRole {
+  carOwner,
+  carRenter,
+}
+
+extension UserRoleExtension on UserRole {
+  String get value {
+    switch (this) {
+      case UserRole.carOwner:
+        return 'carOwner';
+      case UserRole.carRenter:
+        return 'carRenter';
+      default:
+        throw Exception('Invalid JobStatus');
+    }
+  }
+}
