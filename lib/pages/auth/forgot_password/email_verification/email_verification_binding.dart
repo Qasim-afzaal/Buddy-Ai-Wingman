cb69d1a45e6ed391baffdfbcd2b0650d139d700c
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-18 03:40:30 +0500

    add email verification binding

---
+++ lib/pages/auth/forgot_password/email_verification/email_verification_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:buddy_ai_wingman/pages/auth/forgot_password/email_verification/email_verification_controller.dart';
+
+class EmailVerificationBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<EmailVerificationController>(
+      () => EmailVerificationController(),
+    );
+  }
+}
