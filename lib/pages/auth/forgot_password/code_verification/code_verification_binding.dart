036e535f405aff70fed34d0231245037abbcc041
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-18 03:39:20 +0500

    add code verification

---
+++ lib/pages/auth/forgot_password/code_verification/code_verification_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:buddy_ai_wingman/pages/auth/forgot_password/code_verification/code_verification_controller.dart';
+
+class CodeVerificationBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<CodeVerificationController>(
+      () => CodeVerificationController(),
+    );
+  }
+}
