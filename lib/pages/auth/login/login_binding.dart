cf7ac6682f1bb6f2112380ad72d313f70452633b
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-18 03:41:08 +0500

    add login binding

---
+++ lib/pages/auth/login/login_binding.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+import 'package:buddy_ai_wingman/pages/auth/login/login_controller.dart';
+
+import '../../payment/payment_plan/payment_plan_controller.dart';
+import '../../settings/inapp_purchase_source.dart';
+
+class LoginBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<LoginController>(
+      () => LoginController(),
+    );
+  }
+}
