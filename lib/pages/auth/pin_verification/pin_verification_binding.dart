be4c3bedc6b6b3ddbeef17c40482b30568d75485
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-19 02:55:22 +0500

    add pin binding

---
+++ lib/pages/auth/pin_verification/pin_verification_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:buddy_ai_wingman/pages/auth/pin_verification/pin_verification_controller.dart';
+
+class PinVerificationBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<PinVerificationController>(
+      () => PinVerificationController(),
+    );
+  }
+}
