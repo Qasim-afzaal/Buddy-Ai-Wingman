64d1c74bd5bdaa6a7027357ef3fca82faf301f95
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-19 02:56:31 +0500

    add dashborad binding

---
+++ lib/pages/dashboard/dashboard_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:buddy_ai_wingman/pages/dashboard/dashboard_controller.dart';
+
+class DashBoardBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<DashBoardController>(
+      () => DashBoardController(),
+    );
+  }
+}
