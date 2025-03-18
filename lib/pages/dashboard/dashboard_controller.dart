6c78e5b7c116ccf7420174c8ae1a5aaf5fbf3731
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-19 02:56:47 +0500

    handle navigation index of navbar

---
+++ lib/pages/dashboard/dashboard_controller.dart
@@ -0,0 +1,16 @@
+import 'package:buddy_ai_wingman/core/constants/imports.dart';
+
+class DashBoardController extends GetxController {
+
+  int _currentIndex = 0;
+
+  int get currentIndex => _currentIndex;
+
+  void changeNaveIndex(int? index) {
+    if (index != null) {
+      _currentIndex = index;
+      update();
+    }
+  }
+  
+}
