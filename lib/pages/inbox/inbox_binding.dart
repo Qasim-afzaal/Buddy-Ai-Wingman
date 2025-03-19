a8cc030a620b04b8be4c0d4db08a3135f5a1ed17
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-20 03:14:39 +0500

    add inbox binding

---
+++ lib/pages/inbox/inbox_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:buddy_ai_wingman/pages/inbox/inbox_controller.dart';
+
+class InboxBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<InboxController>(
+      () => InboxController(),
+    );
+  }
+}
