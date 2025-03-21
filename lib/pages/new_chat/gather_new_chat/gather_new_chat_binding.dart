dacd4487491a0fca20df72b4e9be2e5c77e5293f
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-22 04:30:10 +0500

    add gether new binding

---
+++ lib/pages/new_chat/gather_new_chat/gather_new_chat_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:buddy_ai_wingman/pages/new_chat/gather_new_chat/gather_new_chat_info_controller.dart';
+
+class GatherNewChatInfoBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<GatherNewChatInfoController>(
+      () => GatherNewChatInfoController(),
+    );
+  }
+}
