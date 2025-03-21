9615e3282286929b4de35dc69fafaa5ad38048bf
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-21 05:06:01 +0500

    add chat binding

---
+++ lib/pages/new_chat/chat/chat_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:buddy_ai_wingman/pages/new_chat/chat/chat_controller.dart';
+
+class ChatBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<ChatController>(
+      () => ChatController(),
+    );
+  }
+}
