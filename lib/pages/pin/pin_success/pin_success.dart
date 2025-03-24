da358434ba7836b8fc403f6d5316deac75d9b463
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-25 04:26:40 +0500

    Add pin Success

---
+++ lib/pages/pin/pin_success/pin_success.dart
@@ -0,0 +1,40 @@
+import 'package:buddy_ai_wingman/core/constants/imports.dart';
+import 'pin_success_controller.dart';
+
+class PinSuccessPage extends StatelessWidget {
+  const PinSuccessPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const SimpleAppBar(
+        title: AppStrings.pinCreated,
+      ),
+      body: GetBuilder<PinSuccessController>(
+        init: PinSuccessController(),
+        builder: (controller) {
+          return Column(
+            children: [
+              SB.h(20),
+              Text(
+                AppStrings.pinCreatedSuccess,
+                style: context.titleLarge?.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SB.h(60),
+              Assets.icons.checkOutline.svg(),
+              SB.h(60),
+
+              AppButton.primary(
+                title: AppStrings.goBackToProfile,
+                onPressed: controller.backToProfile,
+              ),
+
+            ],
+          ).paddingAll(context.paddingDefault);
+        },
+      ),
+    );
+  }
+}
