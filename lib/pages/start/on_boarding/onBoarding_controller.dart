dc48e5728cf103a02893488f39d4b691f2886804
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-30 19:13:51 +0500

    handle is user already login or not logic

---
+++ lib/pages/start/on_boarding/onBoarding_controller.dart
@@ -0,0 +1,56 @@
+import 'package:buddy_ai_wingman/api_repository/api_class.dart';
+import 'package:buddy_ai_wingman/core/constants/imports.dart';
+
+import '../../../routes/app_pages.dart';
+
+class OnBoardingController extends GetxController {
+  Genders? selectedGender;
+  String? selectedAgeRange;
+  Personality? selectedPersonality;
+  RxInt currentPage = 0.obs;
+  final PageController pageController = PageController();
+
+  void onGenderSelection(Genders gender) {
+    selectedGender = gender;
+    update();
+    _nextPage();
+  }
+
+  void onAgeSelection(String age) {
+    selectedAgeRange = age;
+
+    update();
+    _nextPage();
+  }
+
+  void onPersonalitySelection(Personality personality) {
+    selectedPersonality = personality;
+    update();
+  }
+
+  void _nextPage() {
+    pageController.nextPage(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeIn,
+    );
+  }
+
+  void previousPage() {
+    pageController.previousPage(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeIn,
+    );
+  }
+
+  void onFinish() {
+    Get.offNamed(Routes.CREATE_ACCOUNT, arguments: {
+      HttpUtil.gender: selectedGender,
+      HttpUtil.age: selectedAgeRange,
+      HttpUtil.personalityType: selectedPersonality,
+    });
+  }
+
+  void onLogin() {
+    Get.offNamed(Routes.LOGIN);
+  }
+}
