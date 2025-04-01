890a1a41f2857fa04fc57b93f0b715dfd0bb7e3a
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-02 04:43:01 +0500

    add splash page

---
+++ lib/pages/start/splash/splash.dart
@@ -0,0 +1,26 @@
+
+import 'package:buddy_ai_wingman/core/constants/imports.dart';
+import 'splash_controller.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder(
+      init: SplashController(),
+      builder: (controller) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+    
+      body: Center(
+        child: Image.asset(
+          'assets/images/Logi.png',height: 300,width: 300,
+          fit: BoxFit.contain, // Ensures the image is contained within its bounds
+        ),
+      
+    )  );
+      },
+    );
+  }
+}
