63ccaa4459e277a076aa561dddf81449ddedd5c1
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-02 04:42:48 +0500

    add onboard

---
+++ lib/pages/start/on_boarding_for_social/onBoarding_social.dart
@@ -0,0 +1,117 @@
+import 'package:buddy_ai_wingman/core/constants/imports.dart';
+import 'package:buddy_ai_wingman/widgets/age_widget.dart';
+import 'package:buddy_ai_wingman/widgets/gender_widget.dart';
+import 'package:buddy_ai_wingman/widgets/personality_widget.dart';
+
+import 'onBoarding_social_controller.dart';
+
+class OnBoardingSocialPage extends StatelessWidget {
+  const OnBoardingSocialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<OnBoardingSocialController>(
+        init: OnBoardingSocialController(),
+        builder: (controller) {
+          return Scaffold(
+            body: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Obx(
+                    () => Visibility(
+                      visible: controller.currentPage.value > 0,
+                      maintainSize: true,
+                      maintainAnimation: true,
+                      maintainState: true,
+                      child: InkWell(
+                        onTap: controller.previousPage,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Icon(Icons.arrow_back),
+                            SB.w(5),
+                            Text(
+                              AppStrings.back,
+                              style: context.titleMedium?.copyWith(fontWeight: FontWeight.w600),
+                            ),
+                          ],
+                        ).paddingAll(context.paddingDefault),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Obx(
+                        () => _PageIndicator(
+                          isCurrentPage: controller.currentPage.value == 0,
+                        ),
+                      ),
+                      Obx(
+                        () => _PageIndicator(
+                          isCurrentPage: controller.currentPage.value == 1,
+                        ),
+                      ),
+                      Obx(
+                        () => _PageIndicator(
+                          isCurrentPage: controller.currentPage.value == 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SB.h(25),
+                  Expanded(
+                    child: PageView(
+                      controller: controller.pageController,
+                      onPageChanged: (index) => controller.currentPage.value = index,
+                      physics: const NeverScrollableScrollPhysics(),
+                      children: [
+                        GenderWidget(
+                          selectedGender: controller.selectedGender,
+                          onGenderSelection: controller.onGenderSelection,
+                        ),
+                        AgeWidget(
+                          selectedAge: controller.selectedAgeRange,
+                          onAgeSelection: controller.onAgeSelection,
+                        ),
+                        PersonalityWidget(
+                          selectedPersonality: controller.selectedPersonality,
+                          onPersonalitySelection: controller.onPersonalitySelection,
+                          onFinish:(){ controller.onSignup();},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
+
+class _PageIndicator extends StatelessWidget {
+  const _PageIndicator({this.isCurrentPage = false});
+
+  final bool isCurrentPage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 6,
+      width: context.width * (isCurrentPage ? 0.48 : 0.25),
+      // duration: const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: isCurrentPage ? null : context.grey,
+        gradient: isCurrentPage
+            ? LinearGradient(colors: [
+                context.primary,
+                context.secondary,
+              ])
+            : null,
+      ),
+    );
+  }
+}
