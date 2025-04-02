e0e273ba9f8be34211b6c6f5d12a7182ea80a68c
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-03 03:49:14 +0500

    navigate to info page

---
+++ lib/pages/start_sparkd/start_sparkd_controller.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:buddy_ai_wingman/routes/app_pages.dart';
+
+import '../../api_repository/api_class.dart';
+
+class Startbuddy_ai_wingmanController extends GetxController {
+  String? imagePath;
+  void addNewChatManually(bool value) {
+    Get.toNamed(
+      Routes.INFO,
+      arguments: {
+        if (!value) HttpUtil.filePath: imagePath,
+        HttpUtil.isManually: value,
+      },
+    );
+  }
+}
