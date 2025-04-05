951892f408e3375f9596a5cfc775fa50e64d316f
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-06 04:28:48 +0500

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,26 @@
 // import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:sizer/sizer.dart';
 import 'package:buddy_ai_wingman/api_repository/loading.dart';
-import 'package:buddy_ai_wingman/core/constants/helper.dart' show Utils;
+import 'package:buddy_ai_wingman/api_repository/socket_service.dart';
+import 'package:buddy_ai_wingman/core/constants/helper.dart';
+import 'package:buddy_ai_wingman/core/constants/imports.dart';
 import 'package:buddy_ai_wingman/core/theme/theme_light.dart';
 import 'package:buddy_ai_wingman/pages/payment/payment_plan/payment_plan_controller.dart';
 import 'package:buddy_ai_wingman/pages/settings/inapp_purchase_source.dart';
 import 'package:buddy_ai_wingman/routes/app_pages.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:get/instance_manager.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:sizer/sizer.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
-  // await Firebase.initializeApp(); 
+  // await Firebase.initializeApp();
   Loading();
   Utils.screenPortrait();
 //  Get.lazyPut<SocketService>(() => SocketService(), fenix: true);
   runApp(const MyApp());
 }
-
 
 PaymentPlanController getPaymentPlanController() {
   if (Get.isRegistered<PaymentPlanController>()) {
@@ -36,7 +34,8 @@ PaymentPlanController getPaymentPlanController() {
     );
 
     Get.lazyPut<PaymentPlanController>(
-      () => PaymentPlanController(inAppPurchaseSource: Get.find<InAppPurchaseSource>()),
+      () => PaymentPlanController(
+          inAppPurchaseSource: Get.find<InAppPurchaseSource>()),
       fenix: true,
     );
     return Get.find<PaymentPlanController>();
