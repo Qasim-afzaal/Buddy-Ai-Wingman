9f40f62a47c70d3d09a90f7137c780a3f5d17c56
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-07 02:02:42 +0500

    update package

--- lib/pages/auth/forgot_password/email_verification/email_verification.dart
+++ lib/pages/auth/forgot_password/email_verification/email_verification.dart
@@ -1,5 +1,7 @@
 import 'package:google_fonts/google_fonts.dart';
+
 import 'package:buddy_ai_wingman/core/constants/imports.dart';
+import 'package:buddy_ai_wingman/routes/app_pages.dart';
 
 import 'email_verification_controller.dart';
 
@@ -13,29 +15,28 @@ class EmailVerificationPage extends StatelessWidget {
         init: EmailVerificationController(),
         builder: (controller) {
           return Scaffold(
-          
             body: SafeArea(
               child: Column(
                 children: [
                   SB.h(30),
-                  Text(
-                    AppStrings.verification,
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.interTight(
-                            textStyle: const TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),)
-                  ),
+                  Text(AppStrings.verification,
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.interTight(
+                        textStyle: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
                   SB.h(5),
                   Text(
                     AppStrings.verificationDescription,
                     textAlign: TextAlign.center,
-                   style: GoogleFonts.interTight(
-                            textStyle: const TextStyle(
-                              fontSize: 16,
-                              // fontWeight: FontWeight.bold,
-                            ),),
+                    style: GoogleFonts.interTight(
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        // fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                   SB.h(context.height * 0.1),
                   CustomTextField(
