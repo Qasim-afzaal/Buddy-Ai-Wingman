19c920eb8bd05c00c9a5cb5f54e2b3b4749ffe3a
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-07 02:02:29 +0500

    update package

--- lib/pages/auth/forgot_password/create_password/create_password_binding.dart
+++ lib/pages/auth/forgot_password/create_password/create_password_binding.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+
 import 'package:buddy_ai_wingman/pages/auth/forgot_password/create_password/create_password_controller.dart';
 
 class CreatePasswordBinding extends Bindings {
