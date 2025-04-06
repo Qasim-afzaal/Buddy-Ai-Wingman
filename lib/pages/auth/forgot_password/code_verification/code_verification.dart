03b2a58fa5a82f91f61da8acf83165c09243595d
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-07 02:02:25 +0500

    update package

--- lib/pages/auth/forgot_password/code_verification/code_verification.dart
+++ lib/pages/auth/forgot_password/code_verification/code_verification.dart
@@ -1,5 +1,6 @@
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
+
 import 'package:buddy_ai_wingman/core/components/custom_button.dart';
 import 'package:buddy_ai_wingman/core/constants/app_colors.dart';
 import 'package:buddy_ai_wingman/core/constants/imports.dart';
@@ -110,13 +111,12 @@ class CodeVerificationPage extends StatelessWidget {
                               ),
                             ),
                             const SizedBox(width: 10),
-                
+
                             Expanded(
                               child: CustomButton(
-                                onTap: (){
-                                 controller.codeVerification();
+                                onTap: () {
+                                  controller.codeVerification();
                                 },
-                               
                                 buttonText: 'Done',
                                 backGroundColor: AppColors.blackColor,
                                 textColor: AppColors.whiteColor,
@@ -131,57 +131,57 @@ class CodeVerificationPage extends StatelessWidget {
               ),
             ),
           );
-        
-      //     return Scaffold(
-           
-      //       body: SingleChildScrollView(
-      //         child: SafeArea(
-      //           child: Column(
-      //             children: [
-      //               SB.h(30),
-      //               Text(
-      //                 AppStrings.verifyYourMail,
-      //                 textAlign: TextAlign.center,
-      //                 style: context.headlineMedium?.copyWith(
-      //                   color: context.primary,
-      //                   fontWeight: FontWeight.w600,
-      //                 ),
-      //               ),
-      //               SB.h(20),
-      //               Text(
-      //                 AppStrings.verifyYourMailDescription +
-      //                     controller.emailValue!.toString(),
-      //                 textAlign: TextAlign.center,
-      //                 style: context.bodyLarge?.copyWith(
-      //                   fontWeight: FontWeight.w400,
-      //                 ),
-      //               ),
-      //               SB.h(30),
-      //               Text(
-      //                 "The code in the email will only be valid for 10 minutes.\nBe sure to also check your spam folder.",
-      //                 style: TextStyle(
-      //                   fontSize: 14,
-      //                   color: Colors.grey[600],
-      //                 ),
-      //                 textAlign: TextAlign.center,
-      //               ).paddingAll(5),
-      //               SB.h(context.height * 0.05),
-      //               CustomTextField(
-      //                 controller: controller.otpController,
-      //                 keyboardType: TextInputType.number,
-      //                 textInputAction: TextInputAction.done,
-      //                 maxLength: 4,
-      //               ),
-      //               SB.h(5),
-      //               AppButton.primary(
-      //                 title: AppStrings.verify,
-      //                 onPressed: controller.codeVerification,
-      //               )
-      //             ],
-      //           ).paddingAll(context.paddingDefault),
-      //         ),
-      //       ),
-      //     );
+
+          //     return Scaffold(
+
+          //       body: SingleChildScrollView(
+          //         child: SafeArea(
+          //           child: Column(
+          //             children: [
+          //               SB.h(30),
+          //               Text(
+          //                 AppStrings.verifyYourMail,
+          //                 textAlign: TextAlign.center,
+          //                 style: context.headlineMedium?.copyWith(
+          //                   color: context.primary,
+          //                   fontWeight: FontWeight.w600,
+          //                 ),
+          //               ),
+          //               SB.h(20),
+          //               Text(
+          //                 AppStrings.verifyYourMailDescription +
+          //                     controller.emailValue!.toString(),
+          //                 textAlign: TextAlign.center,
+          //                 style: context.bodyLarge?.copyWith(
+          //                   fontWeight: FontWeight.w400,
+          //                 ),
+          //               ),
+          //               SB.h(30),
+          //               Text(
+          //                 "The code in the email will only be valid for 10 minutes.\nBe sure to also check your spam folder.",
+          //                 style: TextStyle(
+          //                   fontSize: 14,
+          //                   color: Colors.grey[600],
+          //                 ),
+          //                 textAlign: TextAlign.center,
+          //               ).paddingAll(5),
+          //               SB.h(context.height * 0.05),
+          //               CustomTextField(
+          //                 controller: controller.otpController,
+          //                 keyboardType: TextInputType.number,
+          //                 textInputAction: TextInputAction.done,
+          //                 maxLength: 4,
+          //               ),
+          //               SB.h(5),
+          //               AppButton.primary(
+          //                 title: AppStrings.verify,
+          //                 onPressed: controller.codeVerification,
+          //               )
+          //             ],
+          //           ).paddingAll(context.paddingDefault),
+          //         ),
+          //       ),
+          //     );
         },
       ),
     );
