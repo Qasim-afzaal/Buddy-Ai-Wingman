6af539ebeb900f42f525af8967b783bf0cb2c66a
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-07 02:01:53 +0500

    update ui

--- lib/core/Widgets/custom_alert_dialogue.dart
+++ lib/core/Widgets/custom_alert_dialogue.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/material.dart';
+
 import 'package:google_fonts/google_fonts.dart';
+
 import 'package:buddy_ai_wingman/core/Utils/assets_util.dart';
 import 'package:buddy_ai_wingman/core/constants/app_colors.dart';
 
@@ -9,20 +10,37 @@ class CustomAlertDialogue extends StatelessWidget {
   final void Function()? onSignOutTapped;
   final bool? showShowContinue;
   final String contextText;
-  const CustomAlertDialogue({super.key, this.onContinueTapped, this.onSignOutTapped, this.showShowContinue, required this.contextText});
+  const CustomAlertDialogue(
+      {super.key,
+      this.onContinueTapped,
+      this.onSignOutTapped,
+      this.showShowContinue,
+      required this.contextText});
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog.adaptive(
       backgroundColor: AppColors.fieldBackgroundColor,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-           Image.asset(CustomImages.notificationBellIcon, width: 124, height: 124,),
-            Text(contextText, textAlign: TextAlign.center, style: GoogleFonts.interTight(textStyle: const TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w500)),),
-          ],
-        ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            CustomImages.notificationBellIcon,
+            width: 124,
+            height: 124,
+          ),
+          Text(
+            contextText,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.interTight(
+                textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500)),
+          ),
+        ],
+      ),
       actions: [
         Row(
           children: [
@@ -33,32 +51,44 @@ class CustomAlertDialogue extends StatelessWidget {
                   alignment: Alignment.center,
                   height: 45,
                   decoration: BoxDecoration(
-                    border: Border.all(width: 1),
-                      borderRadius: BorderRadius.circular(10)
+                      border: Border.all(width: 1),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Text(
+                    "Sign out",
+                    style: GoogleFonts.interTight(
+                        textStyle: const TextStyle(
+                            color: AppColors.blackColor,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12)),
                   ),
-                  child: Text("Sign out", style: GoogleFonts.interTight(textStyle: const TextStyle(color: AppColors.blackColor, fontWeight: FontWeight.w500, fontSize: 12)),),
                 ),
               ),
             ),
-            if(showShowContinue ?? true)
-              ...[
-                const SizedBox(width: 10,),
-                Expanded(
-                  child: GestureDetector(
-                    onTap: onContinueTapped,
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: 45,
-                      decoration: BoxDecoration(
-                          color: AppColors.blackColor,
-                          borderRadius: BorderRadius.circular(10)
-                      ),
-                      child: Text("Continue", style: GoogleFonts.interTight(textStyle: const TextStyle(color: AppColors.whiteColor, fontWeight: FontWeight.w500, fontSize: 12)),),
+            if (showShowContinue ?? true) ...[
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: onContinueTapped,
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 45,
+                    decoration: BoxDecoration(
+                        color: AppColors.blackColor,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text(
+                      "Continue",
+                      style: GoogleFonts.interTight(
+                          textStyle: const TextStyle(
+                              color: AppColors.whiteColor,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 12)),
                     ),
                   ),
                 ),
-              ]
-
+              ),
+            ]
           ],
         ),
       ],
