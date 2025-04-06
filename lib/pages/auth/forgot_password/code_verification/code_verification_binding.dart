512ae06a180b89aa27cc1ab312017c45d82cb154
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-07 02:02:23 +0500

    update package

--- lib/pages/auth/forgot_password/code_verification/code_verification_binding.dart
+++ lib/pages/auth/forgot_password/code_verification/code_verification_binding.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+
 import 'package:buddy_ai_wingman/pages/auth/forgot_password/code_verification/code_verification_controller.dart';
 
 class CodeVerificationBinding extends Bindings {
