991bf6783c8c471533cfb02395ff33ef841d0242
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-10 03:41:47 +0500

    update path

--- lib/pages/new_chat/gather_new_chat/gather_new_chat_binding.dart
+++ lib/pages/new_chat/gather_new_chat/gather_new_chat_binding.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+
 import 'package:buddy_ai_wingman/pages/new_chat/gather_new_chat/gather_new_chat_info_controller.dart';
 
 class GatherNewChatInfoBinding extends Bindings {
