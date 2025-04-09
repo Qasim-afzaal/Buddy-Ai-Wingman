29ec3df8e39d4657a318fca1712e0221af6e8ce3
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-10 03:42:01 +0500

    update path

--- lib/pages/new_chat/gather_new_chat/newbinding.dart
+++ lib/pages/new_chat/gather_new_chat/newbinding.dart
@@ -1,10 +1,13 @@
 import 'package:get/get.dart';
+
 import 'package:buddy_ai_wingman/pages/new_chat/gather_new_chat/controller.dart';
 
-class NewGatherChatInfoBinding extends Bindings {  // Renamed Binding class
+class NewGatherChatInfoBinding extends Bindings {
+  // Renamed Binding class
   @override
   void dependencies() {
-    Get.lazyPut<NewGatherChatInfoController>(  // Renamed Controller class
+    Get.lazyPut<NewGatherChatInfoController>(
+      // Renamed Controller class
       () => NewGatherChatInfoController(),
     );
   }
