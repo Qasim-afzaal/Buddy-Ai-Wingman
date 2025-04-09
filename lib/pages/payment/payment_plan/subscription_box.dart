ca9e13663b5503a4ce35739a1ac7b363dc722119
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-10 03:42:35 +0500

    update path

--- lib/pages/payment/payment_plan/subscription_box.dart
+++ lib/pages/payment/payment_plan/subscription_box.dart
@@ -1,7 +1,7 @@
-
 import 'package:google_fonts/google_fonts.dart';
 import 'package:buddy_ai_wingman/core/constants/app_colors.dart';
 import 'package:buddy_ai_wingman/core/constants/imports.dart';
+
 class SubscriptionBox extends StatelessWidget {
   final String plan;
   final String price;
@@ -22,48 +22,48 @@ class SubscriptionBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onSelect,
-      child:  Container(
-  height: 130,
-  width: MediaQuery.of(context).size.width / 2 - 20,
-  padding: const EdgeInsets.all(16),
-  decoration: BoxDecoration(
-    color: isSelected ? Colors.black : AppColors.textFieldBorderColor,
-    border: Border.all(color: isSelected ? Colors.black : Colors.grey),
-    borderRadius: BorderRadius.circular(8),
-    boxShadow: [
-      BoxShadow(
-        color: Colors.grey.withOpacity(0.2),
-        spreadRadius: 1,
-        blurRadius: 4,
-      ),
-    ],
-  ),
-  child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        price,
-        style: GoogleFonts.interTight(
-          textStyle: TextStyle(
-            fontWeight: FontWeight.w500,
-            fontSize: 14,
-            color: isSelected ? Colors.white : Colors.black87,
-          ),
+      child: Container(
+        height: 130,
+        width: MediaQuery.of(context).size.width / 2 - 20,
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.black : AppColors.textFieldBorderColor,
+          border: Border.all(color: isSelected ? Colors.black : Colors.grey),
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 1,
+              blurRadius: 4,
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              price,
+              style: GoogleFonts.interTight(
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                  color: isSelected ? Colors.white : Colors.black87,
+                ),
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              description,
+              style: GoogleFonts.interTight(
+                textStyle: TextStyle(
+                  fontSize: 12,
+                  color: isSelected ? Colors.white70 : Colors.black54,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-      const SizedBox(height: 4),
-      Text(
-        description,
-        style: GoogleFonts.interTight(
-          textStyle: TextStyle(
-            fontSize: 12,
-            color: isSelected ? Colors.white70 : Colors.black54,
-          ),
-        ),
-      ),
-    ],
-  ),
-),
- );
+    );
   }
 }
