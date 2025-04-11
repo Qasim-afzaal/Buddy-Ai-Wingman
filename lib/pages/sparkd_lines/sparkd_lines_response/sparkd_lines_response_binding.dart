727ccf67f011f9c1ece509a7914eaf5faa97644b
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-12 03:15:51 +0500

    update path

--- lib/pages/sparkd_lines/sparkd_lines_response/sparkd_lines_response_binding.dart
+++ lib/pages/sparkd_lines/sparkd_lines_response/sparkd_lines_response_binding.dart
@@ -1,11 +1,11 @@
-import 'package:buddy_ai_wingman/pages/sparkd_lines/sparkd_lines_response/sparkd_lines_response_controller.dart';
 import 'package:get/get.dart';
+import 'package:buddy_ai_wingman/pages/sparkd_lines/sparkd_lines_response/sparkd_lines_response_controller.dart';
 
-class buddy_ai_wingmanLinesResponseBinding extends Bindings {
+class SparkdLinesResponseBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<buddy_ai_wingmanLinesResponseController>(
-      () => buddy_ai_wingmanLinesResponseController(),
+    Get.lazyPut<SparkdLinesResponseController>(
+      () => SparkdLinesResponseController(),
     );
   }
 }
