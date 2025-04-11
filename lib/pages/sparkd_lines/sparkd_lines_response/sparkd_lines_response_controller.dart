e76538b20118685c1d3a00ce5c30081949b5eef2
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-12 03:15:43 +0500

    update path

--- lib/pages/sparkd_lines/sparkd_lines_response/sparkd_lines_response_controller.dart
+++ lib/pages/sparkd_lines/sparkd_lines_response/sparkd_lines_response_controller.dart
@@ -11,7 +11,7 @@ import '../../home/home_controller.dart';
 import '../../new_chat/chat/get_message_list_response.dart';
 import '../../new_chat/gather_new_chat/spark_line_regenrate_response.dart';
 
-class buddy_ai_wingmanLinesResponseController extends GetxController {
+class SparkdLinesResponseController extends GetxController {
   List<MessageModel> chatList = [];
   SparkLinesModel? message;
   String? name, conversationId;
@@ -93,15 +93,15 @@ class buddy_ai_wingmanLinesResponseController extends GetxController {
             i++) {
           if (sendMessageResponse.formattedMessages![i].senderId !=
               getStorageData.readString(getStorageData.userIdKey)) {
-            list.add(MessageModel(
-              message: sendMessageResponse.formattedMessages![i].content!,
-              messageType: MessageType.text,
-              isReceived: true,
-              headline: null,
-              messageId: sendMessageResponse.formattedMessages![i].id!,
-              isbuddy_ai_wingmanLine: true,
-              mainText: null,
-            ));
+            // list.add(MessageModel(
+            //   message: sendMessageResponse.formattedMessages![i].content!,
+            //   messageType: MessageType.text,
+            //   isReceived: true,
+            //   headline: null,
+            //   messageId: sendMessageResponse.formattedMessages![i].id!,
+            //   isSparkdLine: true,
+            //   mainText: null,
+            // ));
           } else {
             if (message == null) {
               for (int j = 0; j < linesList.length; j++) {
@@ -173,27 +173,27 @@ class buddy_ai_wingmanLinesResponseController extends GetxController {
 
   // Handle button click: Add data immediately if available or run API in the background
   void showPrefetchedMessage() async {
-   if (preFetchedMessage != null && preFetchedMessage!.success!) {
-    // Always update the message at index 0
-    if (chatList.isNotEmpty) {
-      chatList[0] = MessageModel(
-        messageType: MessageType.text,
-        message: preFetchedMessage!.data?.content ?? "",
-        isReceived: true,
-        messageId: preFetchedMessage!.data?.id ?? "",
-        isbuddy_ai_wingmanLine: true,
-      );
-    } else {
-      print("i am add");
-      // If chatList is empty, add the first item
-      chatList.add(MessageModel(
-        messageType: MessageType.text,
-        message: preFetchedMessage!.data?.content ?? "",
-        isReceived: true,
-        messageId: preFetchedMessage!.data?.id ?? "",
-        isbuddy_ai_wingmanLine: true,
-      ));
-    }
+    if (preFetchedMessage != null && preFetchedMessage!.success!) {
+      // Always update the message at index 0
+      if (chatList.isNotEmpty) {
+        // chatList[0] = MessageModel(
+        //   messageType: MessageType.text,
+        //   message: preFetchedMessage!.data?.content ?? "",
+        //   isReceived: true,
+        //   messageId: preFetchedMessage!.data?.id ?? "",
+        //   isSparkdLine: true,
+        // );
+      } else {
+        print("i am add");
+        // If chatList is empty, add the first item
+        // chatList.add(MessageModel(
+        //   messageType: MessageType.text,
+        //   message: preFetchedMessage!.data?.content ?? "",
+        //   isReceived: true,
+        //   messageId: preFetchedMessage!.data?.id ?? "",
+        //   isSparkdLine: true,
+        // ));
+      }
       isFetching.value = false;
       update();
       preFetchedMessage = null;
@@ -206,55 +206,55 @@ class buddy_ai_wingmanLinesResponseController extends GetxController {
   }
 
   // Fetch new message and show loader if necessary
-void fetchNewMessage() async {
-  fetchRequest.value = true; // Indicate user-initiated fetch
-  isFetching.value = true;   // Show loader
+  void fetchNewMessage() async {
+    fetchRequest.value = true; // Indicate user-initiated fetch
+    isFetching.value = true; // Show loader
 
-  var json = {
-    HttpUtil.conversationId: conversationId,
-    HttpUtil.messageId: "",
-    HttpUtil.message: message?.text ?? "",
-    HttpUtil.name: name,
-    HttpUtil.objective: "",
-    HttpUtil.gender: "",
-    HttpUtil.age: "",
-    HttpUtil.personalityType: "",
-    HttpUtil.previousSuggestions: storeList,
-  };
+    var json = {
+      HttpUtil.conversationId: conversationId,
+      HttpUtil.messageId: "",
+      HttpUtil.message: message?.text ?? "",
+      HttpUtil.name: name,
+      HttpUtil.objective: "",
+      HttpUtil.gender: "",
+      HttpUtil.age: "",
+      HttpUtil.personalityType: "",
+      HttpUtil.previousSuggestions: storeList,
+    };
 
-  final data = await APIFunction().patchApiCall2(
-    apiName: Constants.buddy_ai_wingmanLines,
-    withOutFormData: jsonEncode(json),
-  );
+    final data = await APIFunction().patchApiCall2(
+      apiName: Constants.buddy_ai_wingmanLines,
+      withOutFormData: jsonEncode(json),
+    );
 
-  try {
-    SparkLineRegenerateResponse newMessage =
-        SparkLineRegenerateResponse.fromJson(data);
+    try {
+      SparkLineRegenerateResponse newMessage =
+          SparkLineRegenerateResponse.fromJson(data);
 
-    if (newMessage.success!) {
-      // Always update the indexRegenerate position
-      if (indexRegenerate < chatList.length) {
-        chatList[indexRegenerate] = MessageModel(
-          messageType: MessageType.text,
-          message: newMessage.data?.content ?? "",
-          isReceived: true,
-          messageId: newMessage.data?.id ?? "",
-          isbuddy_ai_wingmanLine: true,
-        );
+      if (newMessage.success!) {
+        // Always update the indexRegenerate position
+        if (indexRegenerate < chatList.length) {
+          // chatList[indexRegenerate] = MessageModel(
+          //   messageType: MessageType.text,
+          //   message: newMessage.data?.content ?? "",
+          //   isReceived: true,
+          //   messageId: newMessage.data?.id ?? "",
+          //   isSparkdLine: true,
+          // );
+        }
+        update();
+      } else {
+        utils.showToast(
+            message: newMessage.message ?? "Error fetching new message");
       }
-      update();
-    } else {
-      utils.showToast(
-          message: newMessage.message ?? "Error fetching new message");
+    } catch (e) {
+      ErrorResponse errorModel = ErrorResponse.fromJson(data);
+      utils.showToast(message: errorModel.message!);
+    } finally {
+      isFetching.value = false; // Hide loader
+      fetchRequest.value = false; // Reset user request flag
     }
-  } catch (e) {
-    ErrorResponse errorModel = ErrorResponse.fromJson(data);
-    utils.showToast(message: errorModel.message!);
-  } finally {
-    isFetching.value = false; // Hide loader
-    fetchRequest.value = false; // Reset user request flag
   }
-}
 
   getMeLineStore({String messageId = ""}) async {
     if (isFetching.value) {
@@ -293,35 +293,35 @@ void fetchNewMessage() async {
   }
 
   void chatUpdate(SparkLineRegenerateResponse mainModel, String messageId) {
-    if (mainModel.success!) {
-      if (messageId != "") {
-        indexRegenerate =
-            chatList.indexWhere((item) => item.messageId == messageId);
-        chatList[indexRegenerate] = MessageModel(
-          messageType: MessageType.text,
-          message: mainModel.data?.content ?? "",
-          isReceived: true,
-          messageId: mainModel.data?.id ?? "",
-          isbuddy_ai_wingmanLine: true,
-          headline: null,
-          mainText: null,
-        );
-        update();
-      } else {
-        chatList.add(MessageModel(
-          messageType: MessageType.text,
-          message: mainModel.data?.content ?? "",
-          isReceived: true,
-          messageId: mainModel.data?.id ?? "",
-          isbuddy_ai_wingmanLine: true,
-          headline: null,
-          mainText: null,
-        ));
-        update();
-      }
-    } else {
-      utils.showToast(message: mainModel.message!);
-    }
+    // if (mainModel.success!) {
+    //   if (messageId != "") {
+    //     indexRegenerate =
+    //         chatList.indexWhere((item) => item.messageId == messageId);
+    //     chatList[indexRegenerate] = MessageModel(
+    //       messageType: MessageType.text,
+    //       message: mainModel.data?.content ?? "",
+    //       isReceived: true,
+    //       messageId: mainModel.data?.id ?? "",
+    //       isSparkdLine: true,
+    //       headline: null,
+    //       mainText: null,
+    //     );
+    //     update();
+    //   } else {
+    //     chatList.add(MessageModel(
+    //       messageType: MessageType.text,
+    //       message: mainModel.data?.content ?? "",
+    //       isReceived: true,
+    //       messageId: mainModel.data?.id ?? "",
+    //       isSparkdLine: true,
+    //       headline: null,
+    //       mainText: null,
+    //     ));
+    //     update();
+    //   }
+    // } else {
+    //   utils.showToast(message: mainModel.message!);
+    // }
   }
 
   getMeLine({String messageId = ""}) async {
@@ -342,35 +342,35 @@ void fetchNewMessage() async {
     try {
       SparkLineRegenerateResponse mainModel =
           SparkLineRegenerateResponse.fromJson(data);
-      if (mainModel.success!) {
-        if (messageId != "") {
-          indexRegenerate =
-              chatList.indexWhere((item) => item.messageId == messageId);
-          chatList[indexRegenerate] = MessageModel(
-            messageType: MessageType.text,
-            message: mainModel.data?.content ?? "",
-            isReceived: true,
-            messageId: mainModel.data?.id ?? "",
-            isbuddy_ai_wingmanLine: true,
-            headline: null,
-            mainText: null,
-          );
-          update();
-        } else {
-          chatList.add(MessageModel(
-            messageType: MessageType.text,
-            message: mainModel.data?.content ?? "",
-            isReceived: true,
-            messageId: mainModel.data?.id ?? "",
-            isbuddy_ai_wingmanLine: true,
-            headline: null,
-            mainText: null,
-          ));
-          update();
-        }
-      } else {
-        utils.showToast(message: mainModel.message!);
-      }
+      // if (mainModel.success!) {
+      //   if (messageId != "") {
+      //     indexRegenerate =
+      //         chatList.indexWhere((item) => item.messageId == messageId);
+      //     chatList[indexRegenerate] = MessageModel(
+      //       messageType: MessageType.text,
+      //       message: mainModel.data?.content ?? "",
+      //       isReceived: true,
+      //       messageId: mainModel.data?.id ?? "",
+      //       isSparkdLine: true,
+      //       headline: null,
+      //       mainText: null,
+      //     );
+      //     update();
+      //   } else {
+      //     chatList.add(MessageModel(
+      //       messageType: MessageType.text,
+      //       message: mainModel.data?.content ?? "",
+      //       isReceived: true,
+      //       messageId: mainModel.data?.id ?? "",
+      //       isSparkdLine: true,
+      //       headline: null,
+      //       mainText: null,
+      //     ));
+      //     update();
+      //   }
+      // } else {
+      //   utils.showToast(message: mainModel.message!);
+      // }
     } catch (e) {
       ErrorResponse errorModel = ErrorResponse.fromJson(data);
       utils.showToast(message: errorModel.message!);
