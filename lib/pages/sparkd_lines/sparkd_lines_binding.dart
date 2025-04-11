15d4fd3fe701d2fdd6929cec11ece2ef3a5e7191
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-12 03:15:34 +0500

    update path

--- lib/pages/sparkd_lines/sparkd_lines_binding.dart
+++ lib/pages/sparkd_lines/sparkd_lines_binding.dart
@@ -1,11 +1,11 @@
-import 'package:buddy_ai_wingman/pages/sparkd_lines/sparkd_lines_controller.dart';
 import 'package:get/get.dart';
+import 'package:buddy_ai_wingman/pages/sparkd_lines/sparkd_lines_controller.dart';
 
-class buddy_ai_wingmanLinesBinding extends Bindings {
+class SparkdLinesBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<buddy_ai_wingmanLinesController>(
-      () => buddy_ai_wingmanLinesController(),
+    Get.lazyPut<SparkdLinesController>(
+      () => SparkdLinesController(),
     );
   }
 }
