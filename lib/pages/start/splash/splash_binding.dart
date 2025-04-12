c5e3b0fe28c41562dea55071ec023d790f25f163
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-13 04:21:32 +0500

    refactor code

--- lib/pages/start/splash/splash_binding.dart
+++ lib/pages/start/splash/splash_binding.dart
@@ -1,7 +1,6 @@
 import 'package:get/get.dart';
 import 'package:buddy_ai_wingman/pages/start/splash/splash_controller.dart';
 
-
 class SplashBinding extends Bindings {
   @override
   void dependencies() {
