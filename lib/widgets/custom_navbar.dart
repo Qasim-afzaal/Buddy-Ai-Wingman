9ae54547adeec5d941aba213cadbcbae54d1d642
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-13 04:20:07 +0500

    add custom navbar

--- lib/widgets/custom_navbar.dart
+++ lib/widgets/custom_navbar.dart
@@ -5,7 +5,8 @@ import '../pages/inbox/inbox_controller.dart';
 class CustomBottomNavBar extends StatefulWidget {
   final int selectedIndex;
   final Function(int)? onTabTapped;
-  const CustomBottomNavBar({super.key, this.selectedIndex = 0, this.onTabTapped});
+  const CustomBottomNavBar(
+      {super.key, this.selectedIndex = 0, this.onTabTapped});
 
   @override
   State<CustomBottomNavBar> createState() => _CustomBottomNavBarState();
@@ -20,11 +21,12 @@ class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
       height: 65 + context.paddingBottom,
       padding: const EdgeInsets.symmetric(vertical: 4),
       decoration: BoxDecoration(
-        color: Colors.white,  // Set the background color to white
+        color: Colors.white, // Set the background color to white
         border: Border(
           top: BorderSide(
-            color: Colors.black.withOpacity(0.1),  // Light border color (optional)
-            width: 1,  // Thin border
+            color:
+                Colors.black.withOpacity(0.1), // Light border color (optional)
+            width: 1, // Thin border
           ),
         ),
       ),
@@ -43,14 +45,17 @@ class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
                 }
               },
               child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
                 child: Column(
                   children: [
                     navIconList[i].svg(
                       height: 25.0,
                       color: widget.selectedIndex != i
-                          ? Colors.black  // Default black color for unselected icons
-                          : Colors.black,  // Selected icon color, adjust as needed
+                          ? Colors
+                              .black // Default black color for unselected icons
+                          : Colors
+                              .black, // Selected icon color, adjust as needed
                     ),
                   ],
                 ),
