842f6cfa81c054f19565ff68ac7544820ed6b417
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-12-31 06:32:43 +0500

    chore: update imports.dart

--- lib/core/constants/imports.dart
+++ lib/core/constants/imports.dart
@@ -1,31 +1,31 @@
 export 'package:flutter/material.dart';
 
-export 'package:buddy_ai_wingman/core/constants/app_strings.dart';
-export 'package:buddy_ai_wingman/core/components/sb.dart';
-export 'package:buddy_ai_wingman/gen/assets.gen.dart';
+export 'package:buddy/core/constants/app_strings.dart';
+export 'package:buddy/core/components/sb.dart';
+export 'package:buddy/gen/assets.gen.dart';
 
-export 'package:buddy_ai_wingman/core/components/app_button.dart';
-export 'package:buddy_ai_wingman/core/components/app_bar.dart';
-export 'package:buddy_ai_wingman/core/components/app_text_field.dart';
+export 'package:buddy/core/components/app_button.dart';
+export 'package:buddy/core/components/app_bar.dart';
+export 'package:buddy/core/components/app_text_field.dart';
 
 ///Enums
-export 'package:buddy_ai_wingman/core/enums/genders.dart';
-export 'package:buddy_ai_wingman/core/enums/personality.dart';
+export 'package:buddy/core/enums/genders.dart';
+export 'package:buddy/core/enums/personality.dart';
 
-export 'package:buddy_ai_wingman/core/enums/chat_type.dart';
-export 'package:buddy_ai_wingman/models/message_model.dart';
+export 'package:buddy/core/enums/chat_type.dart';
+export 'package:buddy/models/message_model.dart';
 
 /// Pages=
 
 /// Widgets=
 
 /// Extensions
-export 'package:buddy_ai_wingman/core/extensions/build_context_extension.dart';
+export 'package:buddy/core/extensions/build_context_extension.dart';
 
 /// PACKAGE
 export 'package:get/get.dart'
     hide ContextExtensionss, FormData, MultipartFile, Response;
 export 'package:flutter_svg/flutter_svg.dart';
 
-export 'package:buddy_ai_wingman/core/constants/constants.dart';
-export 'package:buddy_ai_wingman/pages/dashboard/dashboard.dart';
+export 'package:buddy/core/constants/constants.dart';
+export 'package:buddy/pages/dashboard/dashboard.dart';
