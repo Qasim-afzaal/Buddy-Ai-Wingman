982914495db4b757d39f270ca67d8b4ae279b487
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-02 02:27:00 +0500

    chore: update email_verification_binding.dart

--- lib/pages/auth/forgot_password/email_verification/email_verification_binding.dart
+++ lib/pages/auth/forgot_password/email_verification/email_verification_binding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-import 'package:buddy_ai_wingman/pages/auth/forgot_password/email_verification/email_verification_controller.dart';
+import 'package:buddy/pages/auth/forgot_password/email_verification/email_verification_controller.dart';
 
 class EmailVerificationBinding extends Bindings {
   @override
