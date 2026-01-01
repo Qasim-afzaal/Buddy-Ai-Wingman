31fc9a39d7e141c666af6992f70ae207911cc343
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-02 02:26:56 +0500

    chore: update create_password_binding.dart

--- lib/pages/auth/forgot_password/create_password/create_password_binding.dart
+++ lib/pages/auth/forgot_password/create_password/create_password_binding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-import 'package:buddy_ai_wingman/pages/auth/forgot_password/create_password/create_password_controller.dart';
+import 'package:buddy/pages/auth/forgot_password/create_password/create_password_controller.dart';
 
 class CreatePasswordBinding extends Bindings {
   @override
