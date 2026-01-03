4a13f6499931c85e39365ae67690e08ed07fb49f
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-04 03:36:47 +0500

    chore: update pin_verification_binding.dart

--- lib/pages/auth/pin_verification/pin_verification_binding.dart
+++ lib/pages/auth/pin_verification/pin_verification_binding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-import 'package:buddy_ai_wingman/pages/auth/pin_verification/pin_verification_controller.dart';
+import 'package:buddy/pages/auth/pin_verification/pin_verification_controller.dart';
 
 class PinVerificationBinding extends Bindings {
   @override
