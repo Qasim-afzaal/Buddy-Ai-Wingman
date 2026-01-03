d0a0b682a1b05c098a464242fbffe23b136a5e53
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-04 03:37:20 +0500

    chore: update manual_chat_binding.dart

--- lib/pages/home/manual_chat/manual_chat_binding.dart
+++ lib/pages/home/manual_chat/manual_chat_binding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-import 'package:buddy_ai_wingman/pages/home/manual_chat/manual_chat_controller.dart';
+import 'package:buddy/pages/home/manual_chat/manual_chat_controller.dart';
 
 class ManualChatBinding extends Bindings {
   @override
