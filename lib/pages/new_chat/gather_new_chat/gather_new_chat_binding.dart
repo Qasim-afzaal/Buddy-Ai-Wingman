bfdc184058a4f5b368b0269bded7a30ca80c95ac
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-05 05:15:24 +0500

    chore: update gather_new_chat_binding.dart

--- lib/pages/new_chat/gather_new_chat/gather_new_chat_binding.dart
+++ lib/pages/new_chat/gather_new_chat/gather_new_chat_binding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-import 'package:buddy_ai_wingman/pages/new_chat/gather_new_chat/gather_new_chat_info_controller.dart';
+import 'package:buddy/pages/new_chat/gather_new_chat/gather_new_chat_info_controller.dart';
 
 class GatherNewChatInfoBinding extends Bindings {
   @override
