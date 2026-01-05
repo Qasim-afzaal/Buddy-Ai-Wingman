f423228da384a1a56f6925600a4dd1491e1aff4c
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-05 05:15:06 +0500

    chore: update chat_binding.dart

--- lib/pages/new_chat/chat/chat_binding.dart
+++ lib/pages/new_chat/chat/chat_binding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-import 'package:buddy_ai_wingman/pages/new_chat/chat/chat_controller.dart';
+import 'package:buddy/pages/new_chat/chat/chat_controller.dart';
 
 class ChatBinding extends Bindings {
   @override
