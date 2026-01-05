2396695cd3d0c934817df8274208ba4cc1663523
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-05 05:15:12 +0500

    chore: update chat_w_biding.dart

--- lib/pages/new_chat/chat/chat_w_biding.dart
+++ lib/pages/new_chat/chat/chat_w_biding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-import 'package:buddy_ai_wingman/pages/new_chat/chat/chat2_cont.dart';
+import 'package:buddy/pages/new_chat/chat/chat2_cont.dart';
 
 class Chat2Binding extends Bindings {
   @override
