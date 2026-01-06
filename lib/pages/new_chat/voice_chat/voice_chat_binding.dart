4545cff492833d2a77d0f9e5debecac3267706c1
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-06 05:17:56 +0500

    chore: update voice_chat_binding.dart

--- lib/pages/new_chat/voice_chat/voice_chat_binding.dart
+++ lib/pages/new_chat/voice_chat/voice_chat_binding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-import 'package:buddy_ai_wingman/pages/new_chat/voice_chat/voice_chat_controller.dart';
+import 'package:buddy/pages/new_chat/voice_chat/voice_chat_controller.dart';
 
 class VoiceChatBinding extends Bindings {
   @override
