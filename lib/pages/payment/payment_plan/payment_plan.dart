7d56871c9a7a8d074288f9b088bf1a7054895745
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-07 05:28:26 +0500

    chore: update payment_plan.dart

--- lib/pages/payment/payment_plan/payment_plan.dart
+++ lib/pages/payment/payment_plan/payment_plan.dart
@@ -1,11 +1,15 @@
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:buddy_ai_wingman/pages/payment/payment_plan/subscription_box.dart';
-import 'package:buddy_ai_wingman/routes/app_pages.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:buddy_ai_wingman/core/constants/imports.dart';
+
+import 'package:buddy/core/constants/imports.dart';
+import 'package:buddy/pages/payment/payment_plan/payment_plan_controller.dart'
+    show PaymentPlanController;
+import 'package:flutter/cupertino.dart';
+import 'package:buddy/pages/payment/payment_plan/subscription_box.dart';
+import 'package:buddy/routes/app_pages.dart';
+import 'package:buddy/main.dart';
 
 class PaymentPlanPage extends StatefulWidget {
   const PaymentPlanPage({super.key});
@@ -17,6 +21,9 @@ class PaymentPlanPage extends StatefulWidget {
 class _PaymentPlanPageState extends State<PaymentPlanPage> {
   String selectedPlan = ""; // To track the selected subscription plan
 
+  String basicProductId = "com.app.buddy.basic";
+  String proProductId = "com.app.buddy.pro";
+
   void selectPlan(String plan) {
     setState(() {
       selectedPlan = plan;
@@ -24,22 +31,31 @@ class _PaymentPlanPageState extends State<PaymentPlanPage> {
   }
 
   void onSubscription() {
-    Get.offNamed(
+    /*   Get.offNamed(
       Routes.DASHBOARD,
-    );
+    );*/
+    var productID = selectedPlan == "Basic" ? basicProductId : proProductId;
+
+    // Use the existing controller instead of creating a new one
+    var controller = getPaymentPlanController();
+    if (controller.isLoading.value == false) {
+      controller.onSubscriptionPressed(productID);
+    } else {
+      null;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     final List<String> randomTextItems = [
-      "First random text",
-      "Second random text",
-      "Another line of text",
-      "Yet another random text item",
+      "Slimme antwoorden die werken",
+      "Gesprekken in jouw stijl",
+      "Openingszinnen met impact",
+      "Scoor meer dates",
     ];
 
     return Scaffold(
-
+      appBar: const SparkdAppBarBeforePayment(),
       body: SafeArea(
         child: Column(
           children: [
@@ -73,11 +89,11 @@ class _PaymentPlanPageState extends State<PaymentPlanPage> {
                 }).toList(),
               ),
             ),
-            SB.h(context.height * 0.06),
-            const SizedBox(height: 20),
-            SB.h(context.height * 0.06),
+            SB.h(context.height * 0.04),
+            const SizedBox(height: 10),
+            SB.h(context.height * 0.04),
             Text(
-              "Unlimited Content \nWhenever you need it.",
+              "De slimme wingman die 24/7 voor je klaarstaat.",
               textAlign: TextAlign.center,
               style: GoogleFonts.interTight(
                 textStyle: const TextStyle(
@@ -86,9 +102,9 @@ class _PaymentPlanPageState extends State<PaymentPlanPage> {
                 ),
               ),
             ),
-            const SizedBox(height: 5),
+            const SizedBox(height: 10),
             Text(
-              "Choose Plan",
+              "Kies jouw plan.",
               textAlign: TextAlign.center,
               style: GoogleFonts.interTight(
                 textStyle: const TextStyle(
@@ -97,28 +113,29 @@ class _PaymentPlanPageState extends State<PaymentPlanPage> {
                 ),
               ),
             ),
-            const SizedBox(height: 5),
+            const SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 SubscriptionBox(
-                  plan: "Basic",
-                  price: "€9,99",
+                  plan: "Buddy Basic",
+                  price: "€9,99 / maand",
                   description:
-                      "5 AI-powered replies per week and unlimited AI-powered Opening lines.",
+                      "+ 5 slimme AI-antwoorden per week\n+ Onbeperkte openingszinnen die werken\nPerfect voor wie af en toe hulp wil in zijn gesprekken.",
                   isSelected: selectedPlan == "Basic",
                   onSelect: () => selectPlan("Basic"),
                 ),
                 SubscriptionBox(
-                  plan: "Pro",
-                  price: "€14,99",
-                  description: "Unlimited use of features.",
+                  plan: "Buddy Pro",
+                  price: "€14,99 / maand",
+                  description:
+                      "+ Onbeperkte AI-antwoorden en openingszinnen\nVoor wie altijd het perfecte antwoord wil, elke keer weer.",
                   isSelected: selectedPlan == "Pro",
                   onSelect: () => selectPlan("Pro"),
                 ),
               ],
             ),
-            SB.h(20),
+            SB.h(10),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: AppButton.primary(
@@ -126,7 +143,7 @@ class _PaymentPlanPageState extends State<PaymentPlanPage> {
                 onPressed: onSubscription,
               ),
             ),
-            SB.h(10),
+            SB.h(5),
             Text(
               Platform.isIOS
                   ? AppStrings.subscriptionWillRenewIOS
@@ -137,7 +154,7 @@ class _PaymentPlanPageState extends State<PaymentPlanPage> {
             ).paddingAll(4),
             const Spacer(),
             Padding(
-              padding: const EdgeInsets.fromLTRB(20, 8.0, 20, 8),
+              padding: const EdgeInsets.fromLTRB(20, 8.0, 20, 20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -163,7 +180,38 @@ class _PaymentPlanPageState extends State<PaymentPlanPage> {
                   ),
                   GestureDetector(
                     onTap: () {
-                      // Handle restore purchase logic
+                      var controller = PaymentPlanController(
+                          inAppPurchaseSource: Get.find());
+                      controller.onRestorePurchased((p0) {
+                        debugPrint("Verification Api Called Status::$p0");
+                        Map msg = {
+                          "name": "Api Called Navigate base on Status$p0 ..."
+                        };
+                        Constants.socket!.emit("logEvent", msg);
+                        if (p0 == true) {
+                          debugPrint("Navigating to Dashboard...");
+                          Constants.socket!.emit("logEvent", {
+                            "name":
+                                "Verification Done Navigate to Dashboard ..."
+                          });
+                          /*if (getStorageData
+                              .readLoginData()
+                              .data!
+                              .isProfileComplete ==
+                              false) {
+                            Get.offNamed(Routes.ON_BOARDING);
+                          } else {
+                            Get.offNamed(Routes.DASHBOARD);
+                          }*/
+                          Get.offNamed(Routes.DASHBOARD);
+                        } else {
+                          debugPrint("Navigating to Paywall...");
+                          Constants.socket!.emit("logEvent", {
+                            "name": "Verification Done Navigate to PayWall ..."
+                          });
+                          Get.offNamed(Routes.PAYMENT_PLAN);
+                        }
+                      });
                     },
                     child: Text(
                       "Restore",
@@ -201,7 +249,6 @@ class _PaymentPlanPageState extends State<PaymentPlanPage> {
                 ],
               ),
             ),
-            const Spacer(),
           ],
         ),
       ),
