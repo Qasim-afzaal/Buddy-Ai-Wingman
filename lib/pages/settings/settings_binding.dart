d8d0d1e49410961ee364b04a4a0a1e029fd54328
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-07 05:28:59 +0500

    chore: update settings_binding.dart

--- lib/pages/settings/settings_binding.dart
+++ lib/pages/settings/settings_binding.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:buddy_ai_wingman/pages/settings/settings_controller.dart';
+import 'package:buddy/pages/settings/settings_controller.dart';
 
 class SettingsBinding extends Bindings {
   @override
