a022e1de5022e04e5816bfa86e608e750efedf37
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-07 05:28:34 +0500

    chore: update create_pin_binding.dart

--- lib/pages/pin/create_pin/create_pin_binding.dart
+++ lib/pages/pin/create_pin/create_pin_binding.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:buddy_ai_wingman/pages/pin/create_pin/create_pin_controller.dart';
+import 'package:buddy/pages/pin/create_pin/create_pin_controller.dart';
 
 class CreatePinBinding extends Bindings {
   @override
