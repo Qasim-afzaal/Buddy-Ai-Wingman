6905a857361c3ec197cc2f33adfd50e47a6f288c
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-07 05:28:40 +0500

    chore: update pin_success_binding.dart

--- lib/pages/pin/pin_success/pin_success_binding.dart
+++ lib/pages/pin/pin_success/pin_success_binding.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:buddy_ai_wingman/pages/pin/pin_success/pin_success_controller.dart';
+import 'package:buddy/pages/pin/pin_success/pin_success_controller.dart';
 
 class PinSuccessBinding extends Bindings {
   @override
