81ea2c7757a948b7b57f25086f188b18acec437e
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-08 05:23:13 +0500

    chore: update onBoarding_social_binding.dart

--- lib/pages/start/on_boarding_for_social/onBoarding_social_binding.dart
+++ lib/pages/start/on_boarding_for_social/onBoarding_social_binding.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:buddy_ai_wingman/pages/start/on_boarding_for_social/onBoarding_social_controller.dart';
+import 'package:buddy/pages/start/on_boarding_for_social/onBoarding_social_controller.dart';
 
 class OnBoardingSocialBinding extends Bindings {
   @override
