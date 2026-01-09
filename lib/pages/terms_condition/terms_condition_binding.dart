0c3377530ac874f196dd6fd8506a79e1100c400e
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-09 14:51:20 +0500

    chore: update terms_condition_binding.dart

--- lib/pages/terms_condition/terms_condition_binding.dart
+++ lib/pages/terms_condition/terms_condition_binding.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:buddy_ai_wingman/pages/terms_condition/terms_condition_controller.dart';
+import 'package:buddy/pages/terms_condition/terms_condition_controller.dart';
 
 class TermsConditionBinding extends Bindings {
   @override
