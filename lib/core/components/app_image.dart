a1915c96de6fe35fc6e1b0aad2271a2dad22efe6
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-15 06:26:27 +0500

    chore: add documentation to app_image.dart

--- lib/core/components/app_image.dart
+++ lib/core/components/app_image.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+/// App image component
+
 import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:buddy/gen/assets.gen.dart';
