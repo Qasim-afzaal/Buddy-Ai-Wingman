34ee2550d8fdf79aff9307f246dde1625081fee7
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-15 06:25:55 +0500

    chore: add documentation to utils.dart

--- lib/core/Utils/utils.dart
+++ lib/core/Utils/utils.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+/// Utility helper class
 class Utils {
   // function to change the status bar color for the whole app
   changeStatusBarColor() {
