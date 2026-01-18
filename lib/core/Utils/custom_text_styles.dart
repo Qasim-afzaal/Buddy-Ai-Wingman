8d44ec44942a9cd0a1fcc78568b60d3927f4915c
Qasim-afzaal <qasim.afzaal432@gmail.com>
2026-01-19 04:36:07 +0500

    docs: add documentation to bodyTextStyle function

--- lib/core/Utils/custom_text_styles.dart
+++ lib/core/Utils/custom_text_styles.dart
@@ -3,6 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'app_colors.dart';
 
 /// Custom text style utilities
+
+/// Body text style helper function
 bodyTextStyle({double? size = 12, Color color = AppColors.blackColor}) {
   return  GoogleFonts.interTight(
       textStyle: TextStyle(
